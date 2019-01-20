ebb19f951b791c848aa676a76cdf6914dd45b65f
disha kamale <kamaledisha@gmail.com>
2019-01-20 12:57:52 +0530

    Update Cluster1.py

--- Clustering/Cluster1.py
+++ Clustering/Cluster1.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization!
 from scipy.cluster.vq import kmeans2, whiten
 
 
-data = read_point_cloud("/home/dishuuuuu/Point-Cloud-/Dataset/table_scene_lms400_inliers.pcd")
+data = read_point_cloud("./dataset/table_scene_lms400_inliers.pcd")
 
 a = np.asarray(data.points)
 fig = plt.figure()
@@ -15,6 +15,6 @@ x, y = kmeans2(whiten(a), 3, iter = 20)
 print(x)
 print(y)   
 ax.scatter(a[:,0], a[:,1], a[:,2], c=y, alpha=0.33333);
-fig.savefig('/home/dishuu/Desktop/point_plot.png')
+fig.savefig('./Results/point_plot_mug_table.png')
 plt.show()
 
