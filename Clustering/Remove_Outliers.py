44235907bfeaf1241215ba7befc61d5d082b751d
disha kamale <kamaledisha@gmail.com>
2019-01-20 12:56:54 +0530

    Update Remove_Outliers.py

--- Clustering/Remove_Outliers.py
+++ Clustering/Remove_Outliers.py
@@ -4,14 +4,14 @@ import numpy as np
 from scipy.cluster.vq import kmeans2, whiten
 
 
-p = pcl.load("./Dataset/table_scene_lms400.pcd")
+p = pcl.load("./dataset/table_scene_lms400.pcd")
 
 fil = p.make_statistical_outlier_filter()
 fil.set_mean_k(50)
 fil.set_std_dev_mul_thresh(1.0)
 
-pcl.save(fil.filter(), "./Dataset/table_scene_lms400_inliers.pcd")
+pcl.save(fil.filter(), "./dataset/table_scene_lms400_inliers.pcd")
 
 fil.set_negative(True)
-pcl.save(fil.filter(), "./Dataset/table_scene_lms400_outliers.pcd")
+pcl.save(fil.filter(), "./dataset/table_scene_lms400_outliers.pcd")
 
